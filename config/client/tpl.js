4873873d2451a749b29241dad3d16281dfa0d560
狼叔 <langshu.ssl@alibaba-inc.com>
2019-02-27 17:55:36 +0800

    lazycommmit, please ignore

--- config/client/tpl.js
+++
@@ -1,9 +0,0 @@
-import { renderRoutes } from "react-router-config";
-
-ReactDOM.render(
-    <BrowserRouter>
-      {/* kick it all off with the root route */}
-      {renderRoutes(routes)}
-    </BrowserRouter>,
-    document.getElementById("root")
-);
